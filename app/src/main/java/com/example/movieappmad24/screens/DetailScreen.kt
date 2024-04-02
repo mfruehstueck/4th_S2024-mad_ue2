78e80455b5d137faf9b413612239f4fcc7a67552
Manuel Fruehstueck <manuel.fruehstueck@stud.fh-campuswien.ac.at>
2024-04-02 20:08:06 +0200

    fixed some exercise 3 problems

--- app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
@@ -3,17 +3,11 @@ package com.example.movieappmad24.screens
 import android.annotation.SuppressLint
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -23,36 +17,25 @@ import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.example.movieappmad24.R
 import com.example.movieappmad24.models.getMovies
+import com.example.movieappmad24.navigation.BaseScreen
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
 fun DetailScreen(navController: NavController, movieID: String?) {
     val currentMovie = getMovies().find { movie -> movie.id == movieID }
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = { Text("${currentMovie?.title}") },
-                navigationIcon = {
-                    IconButton(onClick = { navController.navigateUp() }) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "Back"
-                        )
-                    }
-                }
-            )
-        },
-        bottomBar = {},
-        content = { innerPadding ->
-            Text("This is movie ${currentMovie?.id}")
-            if (currentMovie != null) {
-                Column (
-                    modifier = Modifier.padding(innerPadding)
-                ){
+
+    currentMovie?.let {
+        BaseScreen(title = it.title,
+            navController = navController,
+            content = { innerPadding ->
+                Text("This is movie ${currentMovie.id}")
+                Column(
+                    modifier = Modifier.padding(innerPadding as PaddingValues)
+                ) {
                     MovieRow(movie = currentMovie)
                     LazyRow {
-                        items(currentMovie.images) {image ->
+                        items(currentMovie.images) { image ->
                             AsyncImage(
                                 model = image,
                                 contentScale = ContentScale.Crop,
@@ -63,6 +46,43 @@ fun DetailScreen(navController: NavController, movieID: String?) {
                     }
                 }
             }
-        }
-    )
+        )
+    }
+
+//    Scaffold(
+//        topBar = {
+//            CenterAlignedTopAppBar(
+//                title = { Text("${currentMovie?.title}") },
+//                navigationIcon = {
+//                    IconButton(onClick = { navController.navigateUp() }) {
+//                        Icon(
+//                            imageVector = Icons.Filled.ArrowBack,
+//                            contentDescription = "Back"
+//                        )
+//                    }
+//                }
+//            )
+//        },
+//        bottomBar = {},
+//        content = { innerPadding ->
+//            Text("This is movie ${currentMovie?.id}")
+//            if (currentMovie != null) {
+//                Column(
+//                    modifier = Modifier.padding(innerPadding)
+//                ) {
+//                    MovieRow(movie = currentMovie)
+//                    LazyRow {
+//                        items(currentMovie.images) { image ->
+//                            AsyncImage(
+//                                model = image,
+//                                contentScale = ContentScale.Crop,
+//                                contentDescription = null,
+//                                placeholder = painterResource(R.drawable.movie_image)
+//                            )
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    )
 }
